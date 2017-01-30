2cc313da076143f54f942b77043c83c83039b2b8
Tommy Tynjä <tommy.tynja@diabol.se>
2017-01-30 14:04:08 +0100

    Fixed proper downstreams in Jenkins job configurations

--- test/jenkins/bootstrap.groovy
+++ test/jenkins/bootstrap.groovy
@@ -3,11 +3,11 @@ import javaposse.jobdsl.dsl.DslFactory
 new GroovyShell().parse(new File("jobs.groovy")).with {
     [
             [name: "happy-service-build", stage: "Commit stage", downstreams: ["happy-service-test"]],
-            [name: "happy-service-test", stage: "Test", downstreams: ["happy-service-test"]],
-            [name: "happy-service-deploy", stage: "Deploy", downstreams: ["happy-service-test"]],
-            [name: "sad-service-build", stage: "Commit stage", downstreams: ["happy-service-test"]],
-            [name: "sad-service-test", stage: "Test", downstreams: ["happy-service-test"]],
-            [name: "sad-service-deploy", stage: "Deploy", downstreams: ["happy-service-test"]]
+            [name: "happy-service-test", stage: "Test", downstreams: ["happy-service-deploy"]],
+            [name: "happy-service-deploy", stage: "Deploy",
+            [name: "sad-service-build", stage: "Commit stage", downstreams: ["sad-service-test"]],
+            [name: "sad-service-test", stage: "Test", downstreams: ["sad-service-deploy"]],
+            [name: "sad-service-deploy", stage: "Deploy"]
     ].each { Map config ->
         createJob(this as DslFactory, config.name, config.stage, config.downstreams)
     }
