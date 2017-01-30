a67ae3c450fb22e799902901dc1f02c525597bb4
Jefferson Girao <jefferson@girao.net>
2017-01-30 16:13:13 +0100

    Cron every minute

--- jenkins/_seed.groovy
+++ jenkins/_seed.groovy
@@ -15,59 +15,59 @@ dir.eachFileRecurse(FileType.FILES) { file ->
 	dslScriptLoader.runScript(file.text)
 }
 
-def createJob(final def dslFactory,
-              final String projectName,
-              final String stageName,
-							final boolean isScheduled,
-              final List<String> downstreamJobs) {
-String schedule = isScheduled ? 'cron("* * * * *")' : ''
-dslScriptLoader.runScript("""\
-  job('$projectName-$stageName') {
-    description '$projectName / $stageName'
-    parameters {
-      stringParam('VERSION', null, null)
-    }
-    deliveryPipelineConfiguration('$stageName', '$stageName')
-    logRotator(-1, 10, -1, -1)
-    triggers {
-      $schedule
-    }
-    steps {
-      shell(
-        '''
-        sleep \$(( ( RANDOM % 10 )  + 1 ))
-        '''
-      )
-    }
-    publishers {
-      downstreamParameterized {
-        configure { node ->
-          downstreamJobs.each() { name ->
-            node << {
-              trigger(name) {
-                delegate.condition('SUCCESS')
-                delegate.parameters {
-                  currentBuild()
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  """.stripIndent())
-}
-
-final boolean SCHEDULED = true
-final boolean NOT_SCHEDULED = false
-[
-        [name: "happy-service-build", stage: "Commit stage", SCHEDULED, downstreams: ["happy-service-test"]],
-        [name: "happy-service-test", stage: "Test", NOT_SCHEDULED, downstreams: ["happy-service-deploy"]],
-        [name: "happy-service-deploy", stage: "Deploy", NOT_SCHEDULED],
-        [name: "sad-service-build", stage: "Commit stage", SCHEDULED, downstreams: ["sad-service-test"]],
-        [name: "sad-service-test", stage: "Test", NOT_SCHEDULED, downstreams: ["sad-service-deploy"]],
-        [name: "sad-service-deploy", stage: "Deploy", NOT_SCHEDULED]
-].each { Map config ->
-    createJob(this as DslFactory, config.name, config.stage, config.downstreams)
-}
+//def createJob(final def dslFactory,
+//              final String projectName,
+//              final String stageName,
+//							final boolean isScheduled,
+//              final List<String> downstreamJobs) {
+//String schedule = isScheduled ? 'cron("* * * * *")' : ''
+//dslScriptLoader.runScript("""\
+//  job('$projectName-$stageName') {
+//    description '$projectName / $stageName'
+//    parameters {
+//      stringParam('VERSION', null, null)
+//    }
+//    deliveryPipelineConfiguration('$stageName', '$stageName')
+//    logRotator(-1, 10, -1, -1)
+//    triggers {
+//      $schedule
+//    }
+//    steps {
+//      shell(
+//        '''
+//        sleep \$(( ( RANDOM % 10 )  + 1 ))
+//        '''
+//      )
+//    }
+//    publishers {
+//      downstreamParameterized {
+//        configure { node ->
+//          downstreamJobs.each() { name ->
+//            node << {
+//              trigger(name) {
+//                delegate.condition('SUCCESS')
+//                delegate.parameters {
+//                  currentBuild()
+//                }
+//              }
+//            }
+//          }
+//        }
+//      }
+//    }
+//  }
+//  """.stripIndent())
+//}
+//
+//final boolean SCHEDULED = true
+//final boolean NOT_SCHEDULED = false
+//[
+//        [name: "happy-service-build", stage: "Commit stage", SCHEDULED, downstreams: ["happy-service-test"]],
+//        [name: "happy-service-test", stage: "Test", NOT_SCHEDULED, downstreams: ["happy-service-deploy"]],
+//        [name: "happy-service-deploy", stage: "Deploy", NOT_SCHEDULED],
+//        [name: "sad-service-build", stage: "Commit stage", SCHEDULED, downstreams: ["sad-service-test"]],
+//        [name: "sad-service-test", stage: "Test", NOT_SCHEDULED, downstreams: ["sad-service-deploy"]],
+//        [name: "sad-service-deploy", stage: "Deploy", NOT_SCHEDULED]
+//].each { Map config ->
+//    createJob(this as DslFactory, config.name, config.stage, config.downstreams)
+//}
--- jenkins/jobdsl/service-1.groovy
+++ jenkins/jobdsl/service-1.groovy
@@ -1,11 +1,31 @@
 pipelineJob('service-1') {
+  triggers {
+    cron("* * * * *")
+  }
   definition {
     cps {
         script("""\
           node('master') {
             stage('build') {
-              sleep 3
+              sh '''
+              #!/bin/bash
+              sleep `echo -n \$(( \$(date +%s) % 10 ))`
+              '''
               sh 'echo built'
+            }
+            stage('test') {
+              sh '''
+              #!/bin/bash
+              sleep `echo -n \$(( \$(date +%s) % 10 ))`
+              '''
+              sh 'echo test'
+            }
+            stage('deploy') {
+              sh '''
+              #!/bin/bash
+              sleep `echo -n \$(( \$(date +%s) % 10 ))`
+              '''
+              sh 'echo deploy'
             }
           }
         """.stripIndent())
