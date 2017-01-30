7544323b8d8faaae2bb1683286ee281a5a7fdf1e
Jefferson Girao <jefferson@girao.net>
2017-01-30 14:35:19 +0100

    Creating some jobs

--- jenkins/_seed.groovy
+++ jenkins/_seed.groovy
@@ -1,5 +1,6 @@
 import jenkins.model.*
 import javaposse.jobdsl.dsl.DslScriptLoader
+import javaposse.jobdsl.dsl.DslFactory
 import javaposse.jobdsl.plugin.JenkinsJobManagement
 import groovy.io.FileType
 
@@ -12,4 +13,54 @@ jm = new JenkinsJobManagement(System.out, [:], new File('.'))
 dslScriptLoader = new DslScriptLoader(jm)
 dir.eachFileRecurse(FileType.FILES) { file ->
 	dslScriptLoader.runScript(file.text)
+}
+
+def createJob(final def dslFactory,
+              final String projectName,
+              final String stageName,
+              final List<String> downstreamJobs) {
+dslScriptLoader.runScript("""\
+  job('$projectName-$stageName') {
+    description '$projectName / $stageName'
+    parameters {
+      stringParam('VERSION', null, null)
+    }
+    deliveryPipelineConfiguration('$stageName', '$stageName')
+    logRotator(-1, 10, -1, -1)
+    steps {
+      shell(
+        '''
+        sleep \$(( ( RANDOM % 10 )  + 1 ))
+        '''
+      )
+    }
+    publishers {
+      downstreamParameterized {
+        configure { node ->
+          downstreamJobs.each() { name ->
+            node << {
+              trigger(name) {
+                delegate.condition('SUCCESS')
+                delegate.parameters {
+                  currentBuild()
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  """.stripIndent())
+}
+
+[
+        [name: "happy-service-build", stage: "Commit stage", downstreams: ["happy-service-test"]],
+        [name: "happy-service-test", stage: "Test", downstreams: ["happy-service-deploy"]],
+        [name: "happy-service-deploy", stage: "Deploy"],
+        [name: "sad-service-build", stage: "Commit stage", downstreams: ["sad-service-test"]],
+        [name: "sad-service-test", stage: "Test", downstreams: ["sad-service-deploy"]],
+        [name: "sad-service-deploy", stage: "Deploy"]
+].each { Map config ->
+    createJob(this as DslFactory, config.name, config.stage, config.downstreams)
 }
--- jenkins/jobdsl/service-1.groovy
+++ jenkins/jobdsl/service-1.groovy
@@ -2,9 +2,11 @@ pipelineJob('service-1') {
   definition {
     cps {
         script("""\
-          stage('build') {
-            sleep 3
-            sh 'echo built'
+          node('master') {
+            stage('build') {
+              sleep 3
+              sh 'echo built'
+            }
           }
         """.stripIndent())
         sandbox()
